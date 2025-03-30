485fc4c50535dee08981cb6221e611113d502247
Osamu Takiya <takiya@toran.sakura.ne.jp>
2025-03-30 12:36:38 +0900

    style: 💄 tests/ 配下 に Biome を適用した（無効化コメントを追記した）

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,16 @@
 import { shallowMount } from '@vue/test-utils'
 import Header from '@/components/Header.vue'
 
+// biome-ignore lint/correctness/noUndeclaredVariables:
 describe('Header.vue', () => {
+  // biome-ignore lint/correctness/noUndeclaredVariables:
   it('renders props.msg when passed', () => {
     const msg = '百英雄伝攻略真書へようこそ！'
     const wrapper = shallowMount(Header, {
       propsData: { msg },
     })
+
+    // biome-ignore lint/correctness/noUndeclaredVariables:
     expect(wrapper.text()).toMatch(msg)
   })
 })
